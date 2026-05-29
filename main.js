49e2de709d6d11abde7eed9ae826bc8b78bbafd1
Rahul <rahulmukeshkumar87@gmail.com>
2026-05-29 17:21:15 +0530

    main version 2.0

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
-const button = document.getElementById("mybutton")
+const button = document.getElementById("photo")
 button.addEventListener("click", function(){
-    // alert("Data saved successfully !!")
-    // document.getElementById("notice").innerHTML = "Congratulations , Now your data safe For saving your data in cloud click the button 2 times !!"
-if(document.getElementById("notice").innerHTML === "Your data in hands of Hackers.If you want to save your data so click the button !!"){
-    alert("Congratulations , Now your data safe")
-        // document.getElementById("notice").innerHTML = "Your data in hands of Hackers.<br> If you want to save your data so click the button !!"
-    document.getElementById("notice").innerHTML = "Congratulations , Now your data safe For saving your data in cloud click the button 2 times !!"
+    
+// if(document.querySelector("image") === "1st.png"){
+// document.getElementById("photo") = "2nd.png"
+// }
+// else{
+//    document.getElementById("photo") = "1st.png"
 
+// }
+
+if(document.querySelector("img").getAttribute("src") === "1st.png"){
+    document.querySelector("img").setAttribute("src", "2nd.png");
+    document.getElementById("notice").innerHTML = "<i>Background Color : Dark Blue</i>"
 }
 else{
-    alert("Data saved into cloud successfully !!")
-    document.getElementById("notice").innerHTML = "Congratulations ,Thanks for using our service."
+    document.querySelector("img").setAttribute("src","1st.png")
+        document.getElementById("notice").innerHTML = "<i>Background Color : Red</i>"
+
+}
+})
+const buttn = document.getElementById("btn")
+buttn.addEventListener("click", function(){
+    
+// if(document.querySelector("image") === "1st.png"){
+// document.getElementById("photo") = "2nd.png"
+// }
+// else{
+//    document.getElementById("photo") = "1st.png"
+
+// }
+
+if(document.querySelector("img").getAttribute("src") === "1st.png"){
+    document.querySelector("img").setAttribute("src", "2nd.png");
+    document.getElementById("notice").innerHTML = "<i>Background Color : Dark Blue</i>"
+}
+else{
+    document.querySelector("img").setAttribute("src","1st.png")
+        document.getElementById("notice").innerHTML = "<i>Background Color : Red</i>"
+
 }
 })
